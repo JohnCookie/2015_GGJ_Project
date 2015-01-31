8bc5da218cb19b7ee246b160911782545d30fa46
Binjie Zhuang <binjie.zhuang@dena.com>
2015-01-31 18:10:55 +0800

    Begin to change to a FPS mode game, but now confused by Vector3 :'(

--- GGJ2015/Assets/Scripts/CameraMgr.cs
+++ GGJ2015/Assets/Scripts/CameraMgr.cs
@@ -9,6 +9,11 @@ public enum CameraStatus{
 	End
 }
 
+public enum GameCameraMode{
+	FPS_Mode,
+	UPDOWN_Mode
+}
+
 public class CameraMgr : MonoBehaviour
 {
 		private static CameraMgr _instance;
@@ -21,59 +26,68 @@ public class CameraMgr : MonoBehaviour
 				return _instance;
 			}
 	
-		public Camera m_playerCamera;
-		public Camera m_uiCamera;
-		public GameObject m_objPlayer;
-		public GameObject m_helperPlayer;
-		public GameObject m_rookieU;
+	public Camera m_playerCamera;
+	public Camera m_uiCamera;
+	public GameObject m_objPlayer;
+	public GameObject m_helperPlayer;
+	public GameObject m_rookieU;
 
 	public GameObject fullView;
 	public GameObject fullLight;
 		
 	public CameraStatus cameraStatus = CameraStatus.FollowPlayer;
+	public GameCameraMode gameCameraMode = GameCameraMode.UPDOWN_Mode;
 
 		// Use this for initialization
 		void Start ()
 		{
-			
 		}
 		
 		// Update is called once per frame
 		void Update ()
 		{
-		switch(cameraStatus){
-			case CameraStatus.FollowPlayer:
-				m_playerCamera.transform.localPosition=new Vector3(m_objPlayer.transform.localPosition.x,800,m_objPlayer.transform.localPosition.z);
-				break;
-			case CameraStatus.FollowHelper:
-				m_playerCamera.transform.localPosition=new Vector3(m_helperPlayer.transform.localPosition.x,800,m_helperPlayer.transform.localPosition.z);
-				break;
-			case CameraStatus.FollowRookie:
-				m_playerCamera.transform.localPosition=new Vector3(m_rookieU.transform.localPosition.x,800,m_rookieU.transform.localPosition.z);
-				break;
-			case CameraStatus.FullLook:	
-				fullView.SetActive(true);
-				fullLight.SetActive(true);
-				if(m_playerCamera.transform.localPosition.y<12000){
-					m_uiCamera.gameObject.SetActive(false);
-					Vector3 currPos=m_playerCamera.transform.localPosition;
-					m_playerCamera.transform.localPosition=new Vector3(currPos.x, currPos.y+Time.deltaTime*400, currPos.z);
-					Vector3 currRotate=m_playerCamera.transform.localRotation.eulerAngles;
-					m_playerCamera.transform.localRotation=Quaternion.Euler(currRotate.x,currRotate.y+Time.deltaTime*10,currRotate.z);
-				}else{
-					m_uiCamera.gameObject.SetActive(true);
-					ItemMgr.getInstance().getItem(0,"runes_1_light");
-					ItemMgr.getInstance().getItem(1,"runes_2_light");
-					ItemMgr.getInstance().getItem(2,"runes_3_light");
-					ItemMgr.getInstance().getItem(3,"runes_4_light");
-					ItemMgr.getInstance().getItem(4,"runes_5_light");
-					HintMgr.getInstance().showHint("To help those beginners like WE used to be");
-					cameraStatus=CameraStatus.End;
-				}
-				break;
-			default:
-				break;
-		}
+			switch(cameraStatus){
+				case CameraStatus.FollowPlayer:
+					if(gameCameraMode==GameCameraMode.UPDOWN_Mode){
+						m_playerCamera.transform.localPosition=new Vector3(m_objPlayer.transform.localPosition.x,800,m_objPlayer.transform.localPosition.z);
+					}
+					if(gameCameraMode==GameCameraMode.FPS_Mode){
+						m_playerCamera.transform.localPosition=m_objPlayer.transform.localPosition;
+						m_playerCamera.transform.localEulerAngles=m_objPlayer.transform.localEulerAngles;
+					}
+					break;
+				case CameraStatus.FollowHelper:
+					m_playerCamera.transform.localRotation=Quaternion.Euler(new Vector3(90,0,0));
+					m_playerCamera.transform.localPosition=new Vector3(m_helperPlayer.transform.localPosition.x,800,m_helperPlayer.transform.localPosition.z);
+					break;
+				case CameraStatus.FollowRookie:
+					m_playerCamera.transform.localRotation=Quaternion.Euler(new Vector3(90,0,0));
+					m_playerCamera.transform.localPosition=new Vector3(m_rookieU.transform.localPosition.x,800,m_rookieU.transform.localPosition.z);
+					break;
+				case CameraStatus.FullLook:	
+					fullView.SetActive(true);
+					fullLight.SetActive(true);
+					if(m_playerCamera.transform.localPosition.y<12000){
+						m_uiCamera.gameObject.SetActive(false);
+						Vector3 currPos=m_playerCamera.transform.localPosition;
+						m_playerCamera.transform.localPosition=new Vector3(currPos.x, currPos.y+Time.deltaTime*400, currPos.z);
+						m_playerCamera.transform.localRotation=Quaternion.Euler(new Vector3(90,0,0));
+						Vector3 currRotate=m_playerCamera.transform.localRotation.eulerAngles;
+						m_playerCamera.transform.localRotation=Quaternion.Euler(currRotate.x,currRotate.y+Time.deltaTime*10,currRotate.z);
+					}else{
+						m_uiCamera.gameObject.SetActive(true);
+						ItemMgr.getInstance().getItem(0,"runes_1_light");
+						ItemMgr.getInstance().getItem(1,"runes_2_light");
+						ItemMgr.getInstance().getItem(2,"runes_3_light");
+						ItemMgr.getInstance().getItem(3,"runes_4_light");
+						ItemMgr.getInstance().getItem(4,"runes_5_light");
+						HintMgr.getInstance().showHint("To help those beginners like WE used to be");
+						cameraStatus=CameraStatus.End;
+					}
+					break;
+				default:
+					break;
+			}
 			
 		}
 }
--- GGJ2015/Assets/Scripts/PlayerController.cs
+++ GGJ2015/Assets/Scripts/PlayerController.cs
@@ -8,68 +8,133 @@ public class PlayerController : MonoBehaviour {
 
 	public float speedScale = 10.0f;
 
+	//tps
 	public float speedX=0;
 	public float speedY=0;
+	//fps
+	public float verticalSpeed=0;
+	public float horizonalSpeed=0;
 
+	//not use now
+	/*
 	private bool lockMoveX=false;
 	private bool lockMoveY=false;
+	*/
+
+	// other version for fps mode
+	public GameCameraMode gameCameraMode = GameCameraMode.UPDOWN_Mode;
+	float screenWidth=1600;
+	float screenHeight=900;
+	float mouseAgiScale = 0.003f;
+
+	void Awake(){
+		CameraMgr.getInstance ().gameCameraMode = gameCameraMode;
+		screenWidth = Screen.width;
+		screenHeight = Screen.height;
+		Debug.Log (screenWidth + "x" + screenHeight);
+	}
 
 	void Update(){
-		if(Input.GetKeyDown(KeyCode.UpArrow)){
-			speedY=1;
-		}
-		if(Input.GetKeyDown(KeyCode.DownArrow)){
-			speedY=-1;
-		}
-		if(Input.GetKeyDown(KeyCode.LeftArrow)){
-			speedX=-1;
-		}
-		if(Input.GetKeyDown(KeyCode.RightArrow)){
-			speedX=1;
+		switch (CameraMgr.getInstance ().gameCameraMode) {
+		case GameCameraMode.UPDOWN_Mode:
+			if(Input.GetKeyDown(KeyCode.UpArrow)){
+				speedY=1;
+			}
+			if(Input.GetKeyDown(KeyCode.DownArrow)){
+				speedY=-1;
+			}
+			if(Input.GetKeyDown(KeyCode.LeftArrow)){
+				speedX=-1;
+			}
+			if(Input.GetKeyDown(KeyCode.RightArrow)){
+				speedX=1;
+			}
+			
+			if(Input.GetKeyUp(KeyCode.UpArrow) && speedY>0){
+				speedY=0;
+			}
+			if(Input.GetKeyUp(KeyCode.DownArrow) && speedY<0){
+				speedY=0;
+			}
+			if(Input.GetKeyUp(KeyCode.LeftArrow) && speedX<0){
+				speedX=0;
+			}
+			if(Input.GetKeyUp(KeyCode.RightArrow) && speedX>0){
+				speedX=0;
+			}
+			gameObject.rigidbody.velocity = new Vector3(speedX*speedScale, 0 , speedY*speedScale);
+			//gameObject.transform.Translate(new Vector3(speedX*speedScale,0,speedY*speedScale));
+			//Vector3 currPosition = gameObject.transform.localPosition;
+			//gameObject.transform.localPosition=new Vector3(currPosition.x+speedX*speedScale, currPosition.y, currPosition.z+speedY*speedScale);
+			
+			if(speedY>0){
+				if(speedX>0){
+					gameObject.transform.rotation= Quaternion.Euler(0,45,0);
+				}else if(speedX==0){
+					gameObject.transform.rotation= Quaternion.Euler(0,0,0);
+				}else{
+					gameObject.transform.rotation= Quaternion.Euler(0,-45,0);
+				}
+			}else if(speedY==0){
+				if(speedX>0){
+					gameObject.transform.rotation= Quaternion.Euler(0,90,0);
+				}else if(speedX==0){
+					gameObject.transform.rotation= gameObject.transform.rotation;
+				}else{
+					gameObject.transform.rotation= Quaternion.Euler(0,-90,0);
+				}
+			}else{
+				if(speedX>0){
+					gameObject.transform.rotation= Quaternion.Euler(0,135,0);
+				}else if(speedX==0){
+					gameObject.transform.rotation= Quaternion.Euler(0,180,0);
+				}else{
+					gameObject.transform.rotation= Quaternion.Euler(0,-135,0);
+				}
+			}
+			break;
+		case GameCameraMode.FPS_Mode:
+			if(Input.GetKeyDown(KeyCode.W)){
+				verticalSpeed=1;
+			}
+			if(Input.GetKeyDown(KeyCode.S)){
+				verticalSpeed=-1;
+			}
+			if(Input.GetKeyDown(KeyCode.A)){
+				horizonalSpeed=-1;
+			}
+			if(Input.GetKeyDown(KeyCode.D)){
+				horizonalSpeed=1;
+			}
+			
+			if(Input.GetKeyUp(KeyCode.W) && verticalSpeed>0){
+				verticalSpeed=0;
+			}
+			if(Input.GetKeyUp(KeyCode.S) && verticalSpeed<0){
+				verticalSpeed=0;
+			}
+			if(Input.GetKeyUp(KeyCode.A) && horizonalSpeed<0){
+				horizonalSpeed=0;
+			}
+			if(Input.GetKeyUp(KeyCode.D) && horizonalSpeed>0){
+				horizonalSpeed=0;
+			}
+
+			//gameObject.rigidbody.velocity = new Vector3(verticalSpeed*speedScale/Mathf.Cos(gameObject.transform.eulerAngles.y),0,verticalSpeed*speedScale/Mathf.Sin(gameObject.transform.eulerAngles.y));
+
+			// Detect mouse position diff with center
+			float offsetOnVertical = Input.mousePosition.y - screenHeight/2;
+			float offsetOnHorizonal = Input.mousePosition.x - screenWidth/2;
+
+			Vector3 currEuler = gameObject.transform.eulerAngles;
+			Vector3 nextEuler = new Vector3(0, currEuler.y+offsetOnHorizonal*mouseAgiScale, currEuler.z);
+			gameObject.transform.eulerAngles=nextEuler;
+
+			break;
+		default:
+			break;
 		}
 
-		if(Input.GetKeyUp(KeyCode.UpArrow) && speedY>0){
-			speedY=0;
-		}
-		if(Input.GetKeyUp(KeyCode.DownArrow) && speedY<0){
-			speedY=0;
-		}
-		if(Input.GetKeyUp(KeyCode.LeftArrow) && speedX<0){
-			speedX=0;
-		}
-		if(Input.GetKeyUp(KeyCode.RightArrow) && speedX>0){
-			speedX=0;
-		}
-		gameObject.rigidbody.velocity = new Vector3(speedX*speedScale, 0 , speedY*speedScale);
-		//gameObject.transform.Translate(new Vector3(speedX*speedScale,0,speedY*speedScale));
-		//Vector3 currPosition = gameObject.transform.localPosition;
-		//gameObject.transform.localPosition=new Vector3(currPosition.x+speedX*speedScale, currPosition.y, currPosition.z+speedY*speedScale);
-
-		if(speedY>0){
-			if(speedX>0){
-				gameObject.transform.rotation= Quaternion.Euler(0,45,0);
-			}else if(speedX==0){
-				gameObject.transform.rotation= Quaternion.Euler(0,0,0);
-			}else{
-				gameObject.transform.rotation= Quaternion.Euler(0,-45,0);
-			}
-		}else if(speedY==0){
-			if(speedX>0){
-				gameObject.transform.rotation= Quaternion.Euler(0,90,0);
-			}else if(speedX==0){
-				gameObject.transform.rotation= gameObject.transform.rotation;
-			}else{
-				gameObject.transform.rotation= Quaternion.Euler(0,-90,0);
-			}
-		}else{
-			if(speedX>0){
-				gameObject.transform.rotation= Quaternion.Euler(0,135,0);
-			}else if(speedX==0){
-				gameObject.transform.rotation= Quaternion.Euler(0,180,0);
-			}else{
-				gameObject.transform.rotation= Quaternion.Euler(0,-135,0);
-			}
-		}
 	}
 //
 //	void OnCollisionEnter(Collision collision){
